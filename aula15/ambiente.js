37f7651aa49bfc413392b3e89d905da7c99efda3
Brendo Alexandre <136013732+brendokame@users.noreply.github.com>
2024-01-02 13:54:45 -0300

    compostos

---
+++ aula15/ambiente.js
@@ -0,0 +1,11 @@
+let num = [5, 8, 2, 9, 3]
+num.sort()
+console.log(num)
+console.log(`o vetor tem ${num.length} posições`)
+let pos = num.indexOf ()
+if (pos == -1) {
+    console.log('o valor não foi encontrado!')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
+
